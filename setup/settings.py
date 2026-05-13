f1ec58fbe5c35266d613e27f61888b098c27b990
Lucas <lucasleite.prog@gmail.com>
2026-05-13 15:25:35 -0300

    adicionado permissões para comunicações com o front-end

--- setup/settings.py
+++ setup/settings.py
@@ -38,11 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'core',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -117,3 +119,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Lista de URLs que podem acessar a API
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "http://localhost:5173", # Caso o front use Vite
+]
+
+# (Opcional) Se quiser liberar geral durante o desenvolvimento, podemos usar:
+# CORS_ALLOW_ALL_ORIGINS = True 
+# Mas lembrar de trocar para a lista específica quando formos fazer o Deploy!
