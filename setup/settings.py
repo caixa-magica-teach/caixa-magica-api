a4ff3d89d94f9c6370f5672313a26f3d99b2d97d
Lucas Leite <lucasleite.prog@gmail.com>
2026-07-30 18:38:53 -0300

    Allow all hosts in settings

--- setup/settings.py
+++ setup/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-pljaaj*)&(97u04*in7+a8u-qm#f)hgkgq!xqphvs$pt%#u^2v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
